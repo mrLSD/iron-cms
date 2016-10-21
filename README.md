b3eb0c290134b7d2f50ff47285898d87278e30b9
Evgeny Ukhanov <mrlsd@ya.ru>
2016-10-21 15:55:13 +0300

    Changed descriptions and added Travis CI

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Iron CMS
+[![Build Status](https://travis-ci.org/mrLSD/iron-cms.svg?branch=master)](https://travis-ci.org/mrLSD/iron-cms) [![License](http://img.shields.io/badge/license-mit-blue.svg?style=flat-square)](https://raw.githubusercontent.com/mrLSD/iron-cms/master/LICENSE)
+
+Inspired by Rust, CMS based on Iron framework
+#### Current status:
+Active development
+
+#### How to build and run:
+```
+$ git clone http://github.com/mrlsd/iron-cms
+$ make release
+$ target/release/iron-cms
+```
+
+#### Requirements:
+* Rust 1.12+
+  
+#### Some useful command:
+* **run:** `make`
+* **build:** `make build`
+* **release build:** `make release`
+
 
 ####License: MIT
--- src/middleware/render.rs
+++ src/middleware/render.rs
@@ -1,3 +1,23 @@
+/// # Render engine middlware
+/// Currently only `HandlebarsEngine` support
+///
+/// ## Features
+/// * Init Tempalte engine
+/// * Adding Template paths
+/// * useful additional helpers with strong params checking
+/// * helpers logger for critical situations
+///
+/// ## Helpers
+/// * `link` - css link helper
+/// * `script` - js  link helper
+///
+/// ## How to use
+/// ```
+/// // Add Template renderer and views path
+/// let paths = vec!["./views/"];
+/// chain.link_after(iron_cms::middleware::template_render(paths));
+/// ```
+/// it's included paths ./views/**/*
 #![allow(dead_code)]
 use hbs::{HandlebarsEngine, DirectorySource};
 use handlebars::{Handlebars, RenderError, RenderContext, Helper, Context};
@@ -6,15 +26,17 @@ use time;
 /// Init Template renderer and add Tempaltes path.
 /// It invoke to after middleware
 pub fn template_render(paths: Vec<&str>) -> HandlebarsEngine {
+    // First init Handlebars
     let mut hregistry = Handlebars::new();
 
-    // Add helpers
+    // Add helpers to Handlebars
     hregistry.register_helper("link", Box::new(link_helper));
     hregistry.register_helper("script", Box::new(script_helper));
 
+    // Our instance HandlebarsEngine depended of Handlebars
     let mut template = HandlebarsEngine::from(hregistry);
 
-    // add a directory source, all files with .html suffix
+    // Add a directory source, all files with .html suffix
     // will be loaded as template
     for path in paths.iter() {
         template.add(Box::new(DirectorySource::new(path, ".html")));
@@ -22,13 +44,14 @@ pub fn template_render(paths: Vec<&str>) -> HandlebarsEngine {
 
     // load templates from all registered sources
     if let Err(r) = template.reload() {
+        // Paniced cause it critical situation
         panic!("{:?}", r);
     }
     template
 }
 
 /// Css link Helper
-/// usege: `{{#link ...}}`
+/// usege: `{{#link ["some/url1", "some/url2"]}}{{/link}}`
 fn link_helper(_: &Context, h: &Helper, _: &Handlebars, rc: &mut RenderContext) -> Result<(), RenderError> {
     let mut css = "".to_owned();
     if h.params().len() == 1 {
@@ -53,7 +76,7 @@ fn link_helper(_: &Context, h: &Helper, _: &Handlebars, rc: &mut RenderContext) 
 }
 
 /// Js link Helper
-/// usege: `{{#script ...}}`
+/// usege: `{{#script ["some/url1", "some/url2"]}}{{/script}}`
 fn script_helper(_: &Context, h: &Helper, _: &Handlebars, rc: &mut RenderContext) -> Result<(), RenderError> {
     let mut js = "".to_owned();
     if h.params().len() == 1 {
@@ -77,8 +100,10 @@ fn script_helper(_: &Context, h: &Helper, _: &Handlebars, rc: &mut RenderContext
     Ok(())
 }
 
+/// Helper logger (currently simple stdout)
+/// Pretty log with Time, Template name, Helper name, Message
 fn helper_log(h: &Helper, rc: &mut RenderContext, message: &str) {
-    println!("{} {:?} |> Helper[{}]: {}",
+    println!("{} \n\t |> Template:{:?} \n\t |> Helper[{}] \n\t |> Message: {}",
                 time::now().strftime("%Y-%m-%d %T").unwrap(),
                 rc.current_template,
                 h.name(),

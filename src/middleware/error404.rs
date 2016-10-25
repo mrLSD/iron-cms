c1c6a06ed733804ce28306f7dedb78d975c9287a
Evgeny Ukhanov <mrlsd@ya.ru>
2016-10-25 13:52:53 +0300

    Change helpers error checking

--- src/middleware/error404.rs
+++ src/middleware/error404.rs
@@ -7,7 +7,7 @@ pub struct Error404;
 
 impl AfterMiddleware for Error404 {
     fn catch(&self, _: &mut Request, err: IronError) -> IronResult<Response> {
-        println!("Catch error: {}", err);
+        println!("Error middleware: \n{:?}\n", err.error);
 
         if let Some(_) = err.error.downcast::<NoRoute>() {
             Ok(Response::with((status::NotFound, "Custom 404 response")))
--- src/middleware/render.rs
+++ src/middleware/render.rs
@@ -55,23 +55,15 @@ pub fn template_render(paths: Vec<&str>) -> HandlebarsEngine {
 /// Css link Helper
 /// usege: `{{#link ["some/url1", "some/url2"]}}{{/link}}`
 fn link_helper(_: &Context, h: &Helper, _: &Handlebars, rc: &mut RenderContext) -> Result<(), RenderError> {
+    let css_links = try!(h.param(0)
+            .and_then(|v| v.value().as_array())
+            .ok_or(RenderError::new("|> link_helper - param 1 with array type is required")));
     let mut css = "".to_owned();
-    if h.params().len() == 1 {
-        let value = h.param(0).unwrap().value();
-        if value.is_array() {
-            let css_links = value.as_array().unwrap();
-            for link in css_links.iter() {
-                if link.is_string() {
-                    css = format!("{}\t<link rel=\"stylesheet\" type=\"text/css\" href=\"{}\">\n", css, link.as_string().unwrap());
-                } else {
-                    helper_log(&h, rc, "wrong array value type - string expected")
-                }
-            }
-        } else {
-            helper_log(&h, rc, "wrong type - array expected")
-        }
-    } else {
-        helper_log(&h, rc, "wrong params count")
+    for link in css_links.iter() {
+        let link = try!(link
+                    .as_string()
+                    .ok_or(RenderError::new("|> link_helper - array param with string type is required")));
+        css = format!("{}\t<link rel=\"stylesheet\" type=\"text/css\" href=\"{}\">\n", css, link);
     }
     try!(rc.writer.write(css.into_bytes().as_ref()));
     Ok(())
@@ -80,23 +72,15 @@ fn link_helper(_: &Context, h: &Helper, _: &Handlebars, rc: &mut RenderContext) 
 /// Js link Helper
 /// usege: `{{#script ["some/url1", "some/url2"]}}{{/script}}`
 fn script_helper(_: &Context, h: &Helper, _: &Handlebars, rc: &mut RenderContext) -> Result<(), RenderError> {
+    let js_links = try!(h.param(0)
+            .and_then(|v| v.value().as_array())
+            .ok_or(RenderError::new("|> script_helper - param 1 with array type is required")));
     let mut js = "".to_owned();
-    if h.params().len() == 1 {
-        let value = h.param(0).unwrap().value();
-        if value.is_array() {
-            let js_links = value.as_array().unwrap();
-            for link in js_links.iter() {
-                if link.is_string() {
-                    js = format!("{}\t<script type=\"text/javascript\" charset=\"utf-8\" src=\"{}\"></script>\n", js, link.as_string().unwrap());
-                } else {
-                    helper_log(&h, rc, "wrong array value type - string expected")
-                }
-            }
-        } else {
-            helper_log(&h, rc, "wrong type - array expected")
-        }
-    } else {
-        helper_log(&h, rc, "wrong params count")
+    for link in js_links.iter() {
+        let link = try!(link
+                    .as_string()
+                    .ok_or(RenderError::new("|> script_helper - array param with string type is required")));
+            js = format!("{}\t<script type=\"text/javascript\" charset=\"utf-8\" src=\"{}\"></script>\n", js, link);
     }
     try!(rc.writer.write(js.into_bytes().as_ref()));
     Ok(())

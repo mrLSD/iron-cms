7c7ce45a0c19d03133b33be894eaaba9c65c2f6b
Evgeny Ukhanov <mrlsd@ya.ru>
2016-11-05 14:20:23 +0200

    Models with Diesel ORM cfg

---
+++ build.rs
@@ -0,0 +1,16 @@
+fn main() {
+    extern crate syntex;
+    extern crate diesel_codegen_syntex as diesel_codegen;
+
+    use std::env;
+    use std::path::Path;
+
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let mut registry = syntex::Registry::new();
+    diesel_codegen::register(&mut registry);
+
+    let src = Path::new("src/lib.in.rs");
+    let dst = Path::new(&out_dir).join("lib.rs");
+
+    registry.expand("", &src, &dst).unwrap();
+}

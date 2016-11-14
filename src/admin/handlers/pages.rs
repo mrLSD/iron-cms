23d5105ec6e1d8465dc6a4182d9ee97d93e76b10
Evgeny Ukhanov <mrlsd@ya.ru>
2016-11-14 17:56:58 +0200

    Validation results and default params

--- src/admin/handlers/pages.rs
+++ src/admin/handlers/pages.rs
@@ -3,24 +3,26 @@ use super::*;
 use iron::modifiers::*;
 use iron::status;
 
-fn default_param() -> BaseDataMap {
-    btreemap! {
+fn default_param(data: &BaseDataMap) -> BaseDataMap {
+    let mut bm = btreemap! {
         "module".to_string() => "pages".to_json(),
-    }
+    };
+    bm.append(&mut data.to_owned());
+    bm
 }
 
 pub fn get_main(_: &mut Request) -> RenderResult {
-    Render::new("admin/pages/index", default_param())
+    Render::new("admin/pages/index", default_param(&btreemap! {}))
 }
 
 pub fn get_create(_: &mut Request) -> RenderResult {
-    Render::new("admin/pages/create", default_param())
+    Render::new("admin/pages/create", default_param(&btreemap! {}))
 }
 
 pub fn get_show(req: &mut Request) -> RenderResult {
     let conn = req.db_conn();
     models::pages::list(&conn);
-    Render::new("admin/pages/create", default_param())
+    Render::new("admin/pages/create", default_param(&btreemap! {}))
 }
 
 pub fn post_create(req: &mut Request) -> RenderResult {
@@ -30,12 +32,13 @@ pub fn post_create(req: &mut Request) -> RenderResult {
     let values = itry!(req.get::<Params>());
     let validate = models::pages::validate(&values);
     if let Some(err) = validate.get_errors() {
-        println!("Validation Errors: {:?}\n\n {:?}", err, validate.to_json());
-        return Render::new("admin/pages/create", default_param());
+        let b = btreemap! {"result".to_string() => validate.to_json()};
+        println!("Validation Errors: {:?}\n\n {:?}", err, validate);
+        return Render::new("admin/pages/create", default_param(&b));
     } else {
         itry!(models::pages::create(&conn, validate.get_values()));
         return Ok(Response::with((status::Found, Redirect(url_for!(req, "admin_pages_main")))))
     }
 
-    Render::new("admin/pages/create", default_param())
+    Render::new("admin/pages/create", default_param(&btreemap! {}))
 }
